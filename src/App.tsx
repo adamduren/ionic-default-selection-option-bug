32573c2aa3f9a2fcc11c756d3488979559b3e46d
Adam Duren <adamduren@gmail.com>
2022-02-02 13:55:32 -0600

    Demonstrate broken ion-select behavior

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,13 @@
-import { IonApp, setupIonicReact } from '@ionic/react';
+import {
+  IonApp,
+  IonContent,
+  IonItem,
+  IonLabel,
+  IonList,
+  IonSelect,
+  IonSelectOption,
+  setupIonicReact,
+} from '@ionic/react';
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
@@ -21,6 +30,66 @@ import './theme/variables.css';
 
 setupIonicReact();
 
-const App: React.FC = () => <IonApp></IonApp>;
+interface IOption {
+  label: string;
+  value: string;
+}
+
+const options: IOption[] = [
+  { label: 'selected by default', value: '1' },
+  { label: 'not selected by default', value: '2' },
+];
+
+const selectedOptions = [options[0]];
+
+const compareWith = (a: IOption, b: IOption) => {
+  return a.value === b.value;
+};
+
+const App: React.FC = () => (
+  <IonApp>
+    <IonContent>
+      <IonList>
+        <IonItem>
+          <IonLabel>Correct: Select using simple values</IonLabel>
+          <IonSelect
+            value={selectedOptions.map((option) => option.value)}
+            placeholder="Select One"
+          >
+            {options.map((option) => (
+              <IonSelectOption key={option.value} value={option.value}>
+                {option.label}
+              </IonSelectOption>
+            ))}
+          </IonSelect>
+        </IonItem>
+        <IonItem>
+          <IonLabel>Correct: Select using objects</IonLabel>
+          <IonSelect value={selectedOptions} placeholder="Select One">
+            {options.map((option) => (
+              <IonSelectOption key={option.value} value={option}>
+                {option.label}
+              </IonSelectOption>
+            ))}
+          </IonSelect>
+        </IonItem>
+        <IonItem>
+          <IonLabel>Incorrect: Select using objects and compareWith</IonLabel>
+          <IonSelect
+            value={selectedOptions}
+            compareWith={compareWith}
+            placeholder="Select One"
+          >
+            {options.map((option) => (
+              <IonSelectOption key={option.value} value={option}>
+                {option.label}
+              </IonSelectOption>
+            ))}
+          </IonSelect>
+        </IonItem>
+      </IonList>
+    </IonContent>
+  </IonApp>
+);
 
 export default App;
