e501f9f85f4c4cde3d48247b6576088233318b3b
Adam Duren <adamduren@gmail.com>
2022-02-03 12:43:49 -0600

    Add another issue with IonSelect multiple

--- src/App.tsx
+++ src/App.tsx
@@ -27,6 +27,7 @@ import '@ionic/react/css/display.css';
 
 /* Theme variables */
 import './theme/variables.css';
+import { useState } from 'react';
 
 setupIonicReact();
 
@@ -40,56 +41,87 @@ const options: IOption[] = [
   { label: 'not selected by default', value: '2' },
 ];
 
-const selectedOptions = [options[0]];
-
 const compareWith = (a: IOption, b: IOption) => {
   return a.value === b.value;
 };
 
-const App: React.FC = () => (
-  <IonApp>
-    <IonContent>
-      <IonList>
-        <IonItem>
-          <IonLabel>Correct: Select using simple values</IonLabel>
-          <IonSelect
-            value={selectedOptions.map((option) => option.value)}
-            placeholder="Select One"
-          >
-            {options.map((option) => (
-              <IonSelectOption key={option.value} value={option.value}>
-                {option.label}
-              </IonSelectOption>
-            ))}
-          </IonSelect>
-        </IonItem>
-        <IonItem>
-          <IonLabel>Correct: Select using objects</IonLabel>
-          <IonSelect value={selectedOptions} placeholder="Select One">
-            {options.map((option) => (
-              <IonSelectOption key={option.value} value={option}>
-                {option.label}
-              </IonSelectOption>
-            ))}
-          </IonSelect>
-        </IonItem>
-        <IonItem>
-          <IonLabel>Incorrect: Select using objects and compareWith</IonLabel>
-          <IonSelect
-            value={selectedOptions}
-            compareWith={compareWith}
-            placeholder="Select One"
-          >
-            {options.map((option) => (
-              <IonSelectOption key={option.value} value={option}>
-                {option.label}
-              </IonSelectOption>
-            ))}
-          </IonSelect>
-        </IonItem>
-      </IonList>
-    </IonContent>
-  </IonApp>
-);
+const App: React.FC = () => {
+  const [selectedOptions, setSelectedOptions] = useState([options[0]]);
+  return (
+    <IonApp>
+      <IonContent>
+        <IonList>
+          <IonItem>
+            <IonLabel>Correct: Select using simple values</IonLabel>
+            <IonSelect
+              multiple
+              value={selectedOptions.map((option) => option.value)}
+              placeholder="Select One"
+            >
+              {options.map((option) => (
+                <IonSelectOption key={option.value} value={option.value}>
+                  {option.label}
+                </IonSelectOption>
+              ))}
+            </IonSelect>
+          </IonItem>
+          <IonItem>
+            <IonLabel>Correct: Select using objects</IonLabel>
+            <IonSelect
+              multiple
+              value={selectedOptions}
+              placeholder="Select One"
+            >
+              {options.map((option) => (
+                <IonSelectOption key={option.value} value={option}>
+                  {option.label}
+                </IonSelectOption>
+              ))}
+            </IonSelect>
+          </IonItem>
+          <IonItem>
+            <IonLabel>Incorrect: Select using objects and compareWith</IonLabel>
+            <IonSelect
+              multiple
+              value={selectedOptions}
+              compareWith={compareWith}
+              placeholder="Select One"
+            >
+              {options.map((option) => (
+                <IonSelectOption key={option.value} value={option}>
+                  {option.label}
+                </IonSelectOption>
+              ))}
+            </IonSelect>
+          </IonItem>
+          <IonItem>
+            <IonLabel>
+              Incorrect: select[multiple] onIonChange event should remain array
+            </IonLabel>
+            <IonSelect
+              multiple
+              value={selectedOptions}
+              placeholder="Select One"
+              onIonChange={(event) => {
+                console.log('array length', event.detail.value.length);
+                console.assert(
+                  Array.isArray(event.detail.value),
+                  `Should be an array: received "${event.detail.value}"`,
+                );
+                setSelectedOptions(event.detail.value);
+              }}
+            >
+              {options.map((option) => (
+                <IonSelectOption key={option.value} value={option}>
+                  {option.label}
+                </IonSelectOption>
+              ))}
+            </IonSelect>
+          </IonItem>
+        </IonList>
+      </IonContent>
+    </IonApp>
+  );
+};
 
 export default App;
